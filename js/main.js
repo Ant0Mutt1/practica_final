d763e7479e404d53d5475fd5cb49a1e6c565d25b
Antonella <antomuttib@gmail.com>
2023-09-14 18:50:07 -0300

    trabajo en responsividad

--- js/main.js
+++ js/main.js
@@ -58,3 +58,26 @@ elementos.forEach((elemento, i )=> elemento.addEventListener("mouseenter", () =>
 ));
 
 elementos.forEach(elemento => elemento.addEventListener("mouseleave", () => div_element.remove()))
+
+elementos.forEach((elemento, i )=> elemento.addEventListener("touchstart", () => {
+    div_element = document.createElement('div');
+    div_tags = document.createElement('div');
+    p = document.createElement('p');
+
+    p.textContent = arr[i]['nombre_proyecto'];
+    div_element.appendChild(p);
+
+    arr[i]['tecnologias'].forEach((tag, j)=>{
+        span_tag = document.createElement('span');    
+        span_tag.textContent = arr[i]['tecnologias'][j];  
+        div_tags.appendChild(span_tag);
+    })
+
+    div_tags.classList.add('trabajos_tag')
+    div_element.appendChild(div_tags)
+    div_element.classList.add('trabajos_info');
+    elemento.appendChild(div_element);
+}
+));
+elementos.forEach(elemento => elemento.addEventListener("touchend", () => div_element.remove()))
+
